f4c080a609b58c06cc885570e04b3f564d635519
Milos Dakic <milos.dakic@dt.com.au>
2017-07-31 11:54:07 +1000

    Simplify module loading

--- resources/assets/js/store/index.js
+++ resources/assets/js/store/index.js
@@ -3,13 +3,12 @@ import Vuex from 'vuex'
 
 import * as actions from './actions'
 import * as getters from './getters'
+import * as modules from './modules'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     actions,
     getters,
-    modules: {
-        stats,
-    },
+    modules,
 })
